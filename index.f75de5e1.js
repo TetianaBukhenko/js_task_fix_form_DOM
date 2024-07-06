bf5c4042812c4eacaf08f5db9eb0a29ee8f06781
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-07-06 15:11:18 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,14 @@
+"use strict";
+const getPlaceholderName = (className)=>{
+    return className.replace(/([a-z])([A-Z])/g, "$1 $2").split(" ").map((el)=>el[0].toUpperCase() + el.slice(1)).join(" ");
+};
+[
+    ...document.querySelectorAll("input")
+].forEach((element)=>{
+    const placeholder = getPlaceholderName(element.getAttribute("name"));
+    const id = element.getAttribute("id");
+    element.setAttribute("placeholder", `${placeholder}`);
+    element.insertAdjacentHTML("beforebegin", `<label class=field-label htmlFor=${id}><strong>${placeholder}</strong></label>`);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
